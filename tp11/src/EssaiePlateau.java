9405e5ad1cfdf726559c6ba2b9b6a436149b13a6
corentin-verquin <corentin.verquin@gmail.com>
2016-03-10 20:21:04 +0100

    10-03

---
+++ tp11/src/EssaiePlateau.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+import java.util.Random;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
+
+public class EssaiePlateau {
+	public static void main(String[] args) {
+		int[][] tableau = new int[10][10];
+		SuperPlateau pl = new SuperPlateau(10);
+		Random rdm = new Random();
+		for (int i = 0; i < tableau.length; i++) {
+			for (int j = 0; j < tableau[0].length; j++) {
+				tableau[i][j] = rdm.nextInt(5);
+			}
+		}
+
+		pl.setJeu(tableau);
+		pl.affichage();
+
+		int rouge = 0, bleu = 0, vert = 0, jaune = 0;
+		for (int i = 0; i < tableau.length; i++) {
+			for (int j = 0; j < tableau[0].length; j++) {
+				if(tableau[i][j]==1){
+					rouge++;
+				}else if(tableau[i][j]==2){
+					bleu++;
+				}else if(tableau[i][j]==3){
+					vert++;
+				}else if(tableau[i][j]==4){
+					jaune++;
+				}
+			}
+		}
+		System.out.println("rouge " + rouge); 
+		System.out.println("bleu " + bleu); 
+		System.out.println("vert "+ vert);
+		System.out.println("jaune " + jaune);
+	}
+}
